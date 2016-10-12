32b91f43683e369353d7b41d47d1aa2b79df29f7
LeeShiyang <15636820686@163.com>
2016-10-12 17:10:22 +0800

    Original work

---
+++ regular_eqation/src/regular_eqation/product_term.java
@@ -0,0 +1,17 @@
+package regular_eqation;
+
+public class product_term {
+	public int coe = 1;
+	public int variable_list[] = new int[26] ;
+	product_term next;
+	
+	public boolean isConst()
+	{
+		for(int i = 0 ; i < 26 ; i++)
+		{
+			if(variable_list[i] != 0)
+				return false;
+		}
+		return true;
+	}
+}
