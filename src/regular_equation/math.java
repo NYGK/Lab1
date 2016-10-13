b00b8c6bf95fb85bbc108115c124c8a7becdfa9e
Chen Yang <Chen Yang>
2016-10-13 17:02:25 +0800

    Version 1.0

---
+++ src/regular_equation/math.java
@@ -0,0 +1,11 @@
+package regular_equation;
+
+public class math {
+	public static int pow(int a , int  b)
+	{
+		int result = 1;
+		for(int i = 0 ;i <b;i++)
+			result *= a ;
+		return result;
+	}
+}
